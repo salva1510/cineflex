59b0f67d8e9eece251bf0736770d52019d9c5479
salva1510 <hjpoffice2@gmail.com>
2026-01-02 12:33:00 +0800

    Update sw.js

--- my-website/sw.js
+++ my-website/sw.js
@@ -1,1 +1,22 @@
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("cineflex-cache").then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/css/home.css",
+        "/js/home.js",
+        "/manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
 

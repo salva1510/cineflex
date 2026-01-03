0486e7a4977aaa990e7997f7f3c98ce8c6c6d48c
salva1510 <hjpoffice2@gmail.com>
2026-01-04 00:50:31 +0800

    Update home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -175,8 +175,8 @@ function changeServer() {
 
       if (server === "vidsrc.cc") {
         embedURL = `https://vidsrc.cc/v2/embed/${type}/${currentItem.id}`;
-      } else if (server === "vidsrc-embed.ru") {
-        embedURL = `https://vidsrc-embed.ru/${type}/?tmdb=${currentItem.id}`;
+      } else if (server === "vsrc.su") {
+        embedURL = `https://vsrc.su/${type}/?tmdb=${currentItem.id}`;
       } else if (server === "player.videasy.net") {
         embedURL = `https://player.videasy.net/${type}/${currentItem.id}`;
       }
@@ -425,6 +425,7 @@ document.getElementById("installBtn")?.addEventListener("click", async () => {
 let currentShow = null;
 let currentSeason = 1;
 let currentEpisode = 1;
+
 
 
 

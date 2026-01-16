766d868a8dbd9883452bf4620f99783307cbcede
salva1510 <hjpoffice2@gmail.com>
2026-01-16 11:34:33 +0800

    Update home.js

--- my-website/home.js
+++ my-website/home.js
@@ -268,7 +268,7 @@ async function searchTMDB() {
 /* =========================
    INITIALIZE
 ========================= */
-showSkeleton("continue-watching-list");
+showSkeleton("continuewatching-list");
 showSkeleton("movies-list");
 showSkeleton("latest-movies-list");
 showSkeleton("top-rated-list");

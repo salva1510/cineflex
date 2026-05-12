2ccfb60ec7bed5443aef9e95d56ad18fabbc1f73
salva1510 <hjpoffice2@gmail.com>
2026-05-12 14:54:43 +0800

    Update home.js

--- my-website/home.js
+++ my-website/home.js
@@ -3,7 +3,7 @@ const BASE_URL = "https://api.themoviedb.org/3";
 const IMG_URL = "https://image.tmdb.org/t/p/w500";
 
 // --- UPDATED PLAYER DOMAIN ---
-const PLAYER_BASE_URL = "https://1embed.cc";
+const PLAYER_BASE_URL = "https://pechify.top";
 
 let currentItem = null;
 let trendingItems = [];

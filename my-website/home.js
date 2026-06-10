0581e6d61bb3c038ff6ff349f986707b7c55676c
salva1510 <hjpoffice2@gmail.com>
2026-06-10 17:26:55 +0800

    Update SERVER_1_URL to new domain

--- my-website/home.js
+++ my-website/home.js
@@ -3,8 +3,7 @@ const BASE_URL = "https://api.themoviedb.org/3";
 const IMG_URL = "https://image.tmdb.org/t/p/w500";
 
 // --- PLAYERS DOMAINS ---
-const SERVER_1_URL = "https://peachify.pro";
-const SERVER_2_URL = "zxcstream.xyz"; // Bagong Server 2 API mo
+const SERVER_1_URL = "zxcstream.xyz";
 
 let currentItem = null;
 let trendingItems = [];

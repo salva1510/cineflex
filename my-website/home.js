a6417bc7a131f804b15d633d4b0bcc8df00e818e
salva1510 <hjpoffice2@gmail.com>
2026-06-19 11:16:35 +0800

    Update SERVER_1_URL to new streaming link

--- my-website/home.js
+++ my-website/home.js
@@ -3,7 +3,7 @@ const BASE_URL = "https://api.themoviedb.org/3";
 const IMG_URL = "https://image.tmdb.org/t/p/w500";
 
 // --- PLAYERS DOMAINS ---
-const SERVER_1_URL = "https://peachify.top";
+const SERVER_1_URL = "https://zxcstream.xyz/player/tv/66732/5/1?dubLang=tl&dubType=0";
 
 let currentItem = null;
 let trendingItems = [];

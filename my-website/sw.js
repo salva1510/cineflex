c85d54a97ac84ecebbbef759ed73e8e32d5b3022
salva1510 <hjpoffice2@gmail.com>
2026-06-28 20:41:10 +0800

    Refactor service worker event listeners

--- my-website/sw.js
+++ my-website/sw.js
@@ -1,13 +1,3 @@
-// Simpleng Service Worker para sa PWA caching requirement
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (e) => {
-  return self.clients.claim();
-});
-
-self.addEventListener('fetch', (e) => {
-  // Pass-through para sa network requests
-  e.respondWith(fetch(e.request));
-});
+self.addEventListener('install', (e) => { self.skipWaiting(); });
+self.addEventListener('activate', (e) => { return self.clients.claim(); });
+self.addEventListener('fetch', (e) => { e.respondWith(fetch(e.request)); });

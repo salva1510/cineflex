732814c8e5630e2615743a4442d852b96e986ba6
salva1510 <hjpoffice2@gmail.com>
2026-06-29 12:55:24 +0800

    Improve error logging for service worker registration
    Change console.log to console.error for service worker registration failure.

--- my-website/home.js
+++ my-website/home.js
@@ -635,14 +635,14 @@ function setupInfiniteScroll() {
         }
     });
 }
-// Register a basic service worker for PWA requirement
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/sw.js')
       .then(reg => console.log('Service Worker Registered!', reg))
-      .catch(err => console.log('Service Worker Registration Failed:', err));
+      .catch(err => console.error('Service Worker Registration Failed:', err));
   });
 }
+
 
 
 init();

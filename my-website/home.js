100c413ab7c86497a5eb148e5f6dae893a911116
salva1510 <hjpoffice2@gmail.com>
2026-06-29 12:49:43 +0800

    Register service worker for PWA
    Added service worker registration for PWA support.

--- my-website/home.js
+++ my-website/home.js
@@ -635,6 +635,15 @@ function setupInfiniteScroll() {
         }
     });
 }
+// Register a basic service worker for PWA requirement
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('Service Worker Registered!', reg))
+      .catch(err => console.log('Service Worker Registration Failed:', err));
+  });
+}
+
 
 init();
 

68e17f9570ac756eed34bd628cde9f5e07f2e9ee
salva1510 <hjpoffice2@gmail.com>
2026-07-01 19:52:15 +0800

    Add Firebase configuration and authentication setup

---
+++ my-website/firebase.js
@@ -0,0 +1,61 @@
+// ================================
+// CINEFLEX FIREBASE ENGINE v2.0
+// ================================
+
+// Firebase Config
+const firebaseConfig = {
+  apiKey: "AIzaSyDdLmGBrgmr8y26GblAhvdcV60eUfPgILk",
+  authDomain: "cineflex-login-b8380.firebaseapp.com",
+  projectId: "cineflex-login-b8380",
+  storageBucket: "cineflex-login-b8380.firebasestorage.app",
+  messagingSenderId: "453926417888",
+  appId: "1:453926417888:web:4c13aefa06f5aed559e785",
+  measurementId: "G-95K8CPHPFM"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Services
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+// Google Provider
+const googleProvider = new firebase.auth.GoogleAuthProvider();
+
+googleProvider.setCustomParameters({
+    prompt: "select_account"
+});
+
+// Persistence
+auth.setPersistence(firebase.auth.Auth.Persistence.LOCAL);
+
+// Current User
+let currentUser = null;
+
+// Listen Login State
+auth.onAuthStateChanged((user) => {
+
+    currentUser = user;
+
+    if (user) {
+
+        console.log("✅ Logged In:", user.displayName);
+
+        window.dispatchEvent(
+            new CustomEvent("cineflex-login", {
+                detail: user
+            })
+        );
+
+    } else {
+
+        console.log("❌ Logged Out");
+
+        window.dispatchEvent(
+            new CustomEvent("cineflex-logout")
+        );
+
+    }
+
+});

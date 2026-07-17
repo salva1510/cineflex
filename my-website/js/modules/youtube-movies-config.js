7b6052238de291dd4f2401ee2412f4da101c6f67
salva1510 <hjpoffice2@gmail.com>
2026-07-18 07:39:34 +0800

    Update YouTube API key in configuration

--- my-website/js/modules/youtube-movies-config.js
+++ my-website/js/modules/youtube-movies-config.js
@@ -3,7 +3,7 @@
  * Restrict HTTP referrers to https://cineflex.online/* and https://www.cineflex.online/*.
  */
 window.CINEFLEX_YOUTUBE = Object.freeze({
-  apiKey: 'AIzaSyBN2H7LUujDgdE2edNkXkCoHljbabNGfyE',
+  apiKey: 'AIzaSyDqaBoK5lBfqWa5AR1EP0hnwtU7B4OUd9c',
   channels: [],
   searchQueries: [
     'full movie official English', 'full movie official Filipino Tagalog',

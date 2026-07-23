fcfbb1a5140ac42b91db3ab7b1c3acbf0d15c1d1
salva1510 <hjpoffice2@gmail.com>
2026-07-23 17:34:59 +0800

    Add files via upload

---
+++ my-website/js/core/build-info300.js
@@ -0,0 +1,13 @@
+(function () {
+  'use strict';
+  const info = Object.freeze({
+    version: '300.8',
+    channel: 'LTS',
+    milestone: 'Plugin Foundation',
+    cacheVersion: 'cineflex-v300-plugin-manager-8',
+    releasedAt: '2026-07-23'
+  });
+  window.CineFlexBuild = info;
+  document.documentElement.dataset.cineflexBuild = info.version;
+  window.dispatchEvent(new CustomEvent('cineflex-build-ready', { detail: info }));
+})();
